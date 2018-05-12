4fc7abef0f7935768ef54a6e2094bcada212fb84
Earl-Fluffy <39085218+Earl-Fluffy@users.noreply.github.com>
2018-05-12 17:29:38 +0200

    Add files via upload

---
+++ src/Menu.java
@@ -0,0 +1,21 @@
+//Taille fenêtre :816, 642
+
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class Menu {
+    public static void main(String[] args) {
+        MyListener start = new MyListener();
+        Clip musique = null;
+        try {
+            musique = AudioSystem.getClip();
+            musique.open(AudioSystem.getAudioInputStream(new File("Ressources/welcome.wav")));
+            musique.start();
+        } catch (Exception exc) {
+            exc.printStackTrace(System.out);
+        }
+    }
+}
+
